c48b9d3305cceb885b3415924085c4be5af4325f
Vladimir Rybin <vladimir.rybin.98@gmail.com>
2025-02-26 15:18:54 +0300

    Configure rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import typescript from 'rollup-plugin-typescript2';
+import terser from "@rollup/plugin-terser";
+
+export default {
+    input: './src/index.ts',
+    output: [
+        {
+            file: "dist/index.cjs.js",
+            format: "cjs",
+            sourcemap: true
+        },
+        {
+            file: "dist/index.esm.js",
+            format: "esm",
+            sourcemap: true
+        }
+    ],
+
+    plugins: [
+        typescript({declarationDir: 'dist'}),
+        terser()
+    ],
+};
